154ebed1276513997c15d56554dc085a25c25258
firas1996 <firas.hm.1996@gmail.com>
2026-03-02 10:52:27 +0100

    a

--- middlewares/authGuardMW.js
+++ middlewares/authGuardMW.js
@@ -1,9 +1,41 @@
+const { verify } = require("jsonwebtoken");
+const { promisify } = require("util");
+const User = require("../models/userModel");
+
 exports.protectorMW = async (req, res, next) => {
   try {
+    let token;
     // 1) bech t'thabat si el user 3andou token or not !
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      res.status(401).json({
+        message: "You are not logged in !!!",
+      });
+    }
     // 2) nech nthabat ken el token valid or not !
+    const ourToken = await promisify(verify)(token, process.env.JWT_SECRET);
+    console.log(ourToken);
     // 3) bech nthabat si el user still exists !
+    const user = await User.findById(ourToken.id);
+    if (!user) {
+      res.status(404).json({
+        message: "User no longer exists !!!",
+      });
+    }
     // 4) bech nthabat ken el token tsan3et 9bal e5er pass update !
+    console.log(parseInt(user.last_pass_change_date.getTime() / 1000));
+    console.log(ourToken.iat);
+    if (parseInt(user.last_pass_change_date.getTime() / 1000) > ourToken.iat) {
+      res.status(401).json({
+        message: "Token expired !!!",
+      });
+    }
+    next();
   } catch (error) {
     res.status(400).json({
       message: "Failed !!!",
@@ -11,3 +43,5 @@ exports.protectorMW = async (req, res, next) => {
     });
   }
 };
+
+exports.checkRoleMW = async (params) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,15 +6,19 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../controllers/userController");
+const { protectorMW, checkRoleMW } = require("../middlewares/authGuardMW");
 const router = require("express").Router();
 
 router.post("/signUp", signUp);
 router.post("/signIn", signIn);
 
-router.route("/users").post(createUser).get(getUsers);
+router
+  .route("/users")
+  .post(createUser)
+  .get(protectorMW, checkRoleMW("admin"), getUsers);
 router
   .route("/users/:id")
-  .get(getUserById)
+  .get(checkRoleMW("admin", "user"), getUserById)
   .patch(updateUserById)
   .delete(deleteUserById);
 
